fa081ea1a26bb2d2d02a94b24bdc5e3fda102e88
Bregraal <103366492+Bregraal@users.noreply.github.com>
2023-06-12 09:27:59 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,125 @@
+(function() {
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let header = select('#header')
+    let offset = header.offsetHeight
+
+    if (!header.classList.contains('header-scrolled')) {
+      offset -= 16
+    }
+
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: 'smooth'
+    })
+  }
+
+  /**
+   * Back to top button
+   */
+  let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
+
+  /**
+   * Mobile nav toggle
+   */
+  on('click', '.mobile-nav-toggle', function(e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('fa-list')
+    this.classList.toggle('fa-x')
+  })
+
+  /**
+   * Mobile nav dropdowns activate
+   */
+  on('click', '.navbar .dropdown > a', function(e) {
+    if (select('#navbar').classList.contains('navbar-mobile')) {
+      e.preventDefault()
+      this.nextElementSibling.classList.toggle('dropdown-active')
+    }
+  }, true)
+
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  on('click', '.scrollto', function(e) {
+    if (select(this.hash)) {
+      e.preventDefault()
+
+      let navbar = select('#navbar')
+      if (navbar.classList.contains('navbar-mobile')) {
+        navbar.classList.remove('navbar-mobile')
+        let navbarToggle = select('.mobile-nav-toggle')
+        navbarToggle.classList.toggle('fa-solid fa-list')
+        navbarToggle.classList.toggle('fa-solid fa-x')
+      }
+      scrollto(this.hash)
+    }
+  }, true)
+
+  /**
+   * model carousel indicators
+   */
+  let modelCarouselIndicators = select("#model-carousel-indicators")
+  let modelCarouselItems = select('#modelCarousel .carousel-item', true)
+
+  modelCarouselItems.forEach((item, index) => {
+    (index === 0) ?
+    modelCarouselIndicators.innerHTML += "<li data-bs-target='#modelCarousel' data-bs-slide-to='" + index + "' class='active'></li>":
+      modelCarouselIndicators.innerHTML += "<li data-bs-target='#modelCarousel' data-bs-slide-to='" + index + "'></li>"
+  });
+
+})()
+
+function redirect(idNum, name){
+  localStorage.setItem("name", name);
+  var destinationPage = "Map.html?data=" + encodeURIComponent(idNum);
+  window.location.href = destinationPage;
+}
